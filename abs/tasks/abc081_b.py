9b29c9fd318f0337db1a4007a600d4aac029e33d
fkyohei <kyohei19921016@gmail.com>
2019-01-28 22:39:50 +0900

    add abs abc081_b

---
+++ abs/tasks/abc081_b.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(input())
+# 内包表記
+input_list = [int(_) for _ in input().split()]
+
+count = 0
+while True:
+    for index in range(N):
+        if input_list[index] % 2 == 0:
+            input_list[index] /= 2
+        else:
+            print(count)
+            sys.exit()
+    count += 1
