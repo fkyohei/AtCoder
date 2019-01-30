5df4ac7f0f05c29c36729d3dce8a9418edb6f074
fkyohei <kyohei19921016@gmail.com>
2019-01-30 22:06:03 +0900

    add abs abc083_b

---
+++ abs/tasks/abc083_b.py
@@ -0,0 +1,12 @@
+N, A, B = map(int, input().split())
+
+total = 0
+for n in range(N + 1):
+    if n < A:
+        continue
+    n_list = [int(_) for _ in list(str(n))]
+    n_sum = sum(n_list)
+    if A <= n_sum <= B:
+        total += n
+
+print(total)
