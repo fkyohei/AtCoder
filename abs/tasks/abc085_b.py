3b9d9c9e710c18942894b5e05e2e8943cb6292dd
fkyohei <kyohei19921016@gmail.com>
2019-01-31 21:47:24 +0900

    add abs abc085_b

---
+++ abs/tasks/abc085_b.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+d_list = []
+for n in range(N):
+    d_list.append(int(input()))
+
+d_list.sort(reverse=True)
+
+count = 1
+top = d_list[0]
+for d in d_list:
+    if d >= top:
+        continue
+    top = d
+    count += 1
+
+print(count)
+
+## 並び変えなくても重複排除して個数を数えれば同じ答えが得られる
+## リストの重複排除はset(変数名)
